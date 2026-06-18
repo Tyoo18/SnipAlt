8d26f1f22d5d348935efbdcc1df74be1af5cf9ed
Tyoo18 <aldo.rahmadan59@smk.belajar.id>
2026-06-18 15:02:04 +0700

    chore: resolve dist conflict by removing from track

--- dist/vendor/.vite-deps-react.js__v--ca7c2b5e.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react
-} from "/vendor/.vite-deps-chunk-DRWLMN53.js__v--ca7c2b5e.js";
-import "/vendor/.vite-deps-chunk-G3PMV62Z.js__v--ca7c2b5e.js";
-export default require_react();
-//# sourceMappingURL=react.js.map
